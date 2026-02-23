02fc6e3dff5172caea9c927d5654a09072f332d9
shpeta04 <shpetimhoti0@gmail.com>
2026-02-23 14:27:56 +0100

    Mobile view changes

--- resources/views/admin/testdrives/index.blade.php
+++ resources/views/admin/testdrives/index.blade.php
@@ -8,7 +8,9 @@
             Test Drive Requests
         </h1>
 
-        <div class="bg-neutral-900 rounded-xl overflow-hidden">
+
+        {{-- DESKTOP TABLE --}}
+        <div class="bg-neutral-900 rounded-xl overflow-hidden hidden lg:block">
 
             <table class="w-full text-left">
 
@@ -29,7 +31,7 @@
 
                 @foreach($testdrives as $request)
 
-                    <tr class="border-b border-neutral-800">
+                    <tr class="border-b border-neutral-800 hover:bg-neutral-800 transition">
 
                         <td class="p-4 text-white">
                             {{ $request->car->brand }} {{ $request->car->model }}
@@ -68,6 +70,58 @@
             </table>
 
         </div>
+
+
+        {{-- MOBILE CARD VIEW --}}
+        <div class="lg:hidden space-y-4">
+
+            @foreach($testdrives as $request)
+
+                <div class="bg-neutral-900 border border-neutral-800 rounded-xl p-4">
+
+                    <div class="text-white font-semibold text-lg mb-2">
+                        {{ $request->car->brand }} {{ $request->car->model }}
+                    </div>
+
+                    <div class="text-neutral-400 text-sm">
+                        {{ $request->name }}
+                    </div>
+
+                    <div class="text-neutral-400 text-sm">
+                        {{ $request->email }}
+                    </div>
+
+                    <div class="text-neutral-400 text-sm">
+                        {{ $request->phone }}
+                    </div>
+
+                    <div class="text-neutral-400 text-sm mt-2">
+                        Preferred Date:
+                        <span class="text-white">
+                        {{ $request->preferred_date }}
+                    </span>
+                    </div>
+
+
+                    <form method="POST"
+                          action="{{ route('admin.testdrives.destroy', $request) }}"
+                          class="mt-4">
+
+                        @csrf
+                        @method('DELETE')
+
+                        <button class="w-full bg-red-500 py-2 rounded hover:bg-red-600 transition">
+                            Delete Request
+                        </button>
+
+                    </form>
+
+                </div>
+
+            @endforeach
+
+        </div>
+
 
     </div>
 
